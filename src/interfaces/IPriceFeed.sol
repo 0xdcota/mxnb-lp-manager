8e9cc4be5aa8c18cd4c28c8f56367ff50a22be2a
0xdcota <32775237+0xdcota@users.noreply.github.com>
2025-07-06 16:44:02 -0700

    chore: deployments mxnblpmanager arbitrum

--- src/interfaces/IPriceFeed.sol
+++ src/interfaces/IPriceFeed.sol
@@ -16,9 +16,4 @@ interface IPriceFeed {
         external
         view
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-    // function getRoundData(uint80 _roundId)
-    //     external
-    //     view
-    //     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
